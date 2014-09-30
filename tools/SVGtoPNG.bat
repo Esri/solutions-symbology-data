70869d5b7df4885869d41007ea799ce6209c69b8
C Moore <csmooreacm@acm.org>
2014-09-30 06:57:11 -0400

    #3, + creating combined & pro styles
    Improvements:
    
    1. Created style/stylx with symbol sets combined (in
    solutions-symbology-combined folder)
    2. Also (a) added missing category/tags to styles, (b) changed source
    image to vector/emf (so it could be scaled) and (c) set the default size
    to an enlarged value of 32
    3. Included/added emf/vector source images
    4. Readme improvements from #3 & moved unused image jpgs to doc folder

--- tools/SVGtoPNG.bat
+++ tools/SVGtoPNG.bat
@@ -4,10 +4,10 @@
 :: For Inkscape help, see http://inkscape.org/doc/inkscape-man.html
 :: For an alternative to Inkscape that works just as well, see http://code.google.com/p/svg2emf/
  
-:: Correct paths below
-SET converter=C:\Program Files (x86)\Inkscape\inkscape.com
-SET source_folder=C:\git\solutions-symbology-data\data\incident-symbols\svgs
-SET destination_folder=C:\git\solutions-symbology-data\data\incident-symbols\pngs
+:: :: --> TODO: Correct paths below (to your local configuration/setup)
+SET converter=C:\{Install Location}\Inkscape\inkscape.com
+SET source_folder=C:\{Github}\solutions-symbology-data\data\incident-symbols\svgs
+SET destination_folder=C:\{Github}\solutions-symbology-data\data\incident-symbols\pngs
 
 :: Use this option if you want to convert a folder and all subfolders (recursion) ...
 for /r "%source_folder%" %%i in (*.svg) do "%converter%" "%%i" --export-width=64 --export-height=64 --export-png="%destination_folder%\%%~ni.png"
